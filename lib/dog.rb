6ed6ef2f9db11e7f93dbaab693813c3e7bfdcfbd
Maryna Voitenko <maryna.voitenko@gmail.com>
2017-11-04 22:29:23 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,7 +1,15 @@
 class Dog
+  attr_accessor :name
 
   @all = []
-  def initialize
+  def initialize(name)
+    @name = name
     @all << self
+  end
+
+  def self.all
+    @all.each do |puppy|
+      puts puppy
+    end
   end
 end
